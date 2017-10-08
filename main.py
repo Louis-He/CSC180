53b7f5ca4e06ba09bd3d6d4b81bf33415a871c5a
Louis-He <1726110778@qq.com>
2017-10-08 15:12:47 -0400

    program completed

--- main.py
+++ main.py
@@ -37,28 +37,49 @@ print "INFO: FOUT", FOUT
 print "INFO: COL",COL
 print "INFO: DIR",DIR
 
+if DIR == '+':
+    up = True
+else:
+    up = False
 
+
+def genSortKey(col,up):
+    def key(x):
+        return x[int(col)] if up else -x[int(col)]
+    return key
+sortKey = genSortKey(COL, up)
+
+#INPUT
 try:
     f = open(FIN, 'r')
     lines = f.readlines()
     f.close()
-    accum = []
-    for i in lines:
-        j = i.split('\n')[0]
-        k=j.split(',')
-        r=[]
-        for x in k:
-            r = r +[int(x)]
-        accum = accum + [r]
-    print accum
-
-    try:
-        g = open('dataout', 'w')
-        g.write('This is content\n')
-        g.write('This is more content\n')
-    except:
-        print 'ERR: cannot write to file "dataout"'
-
 except:
     print 'ERR: file DO NOT EXIST.'
+
+#PROCESS
+accum = []
+for i in lines:
+    j = i.split('\n')[0]
+    k=j.split(',')
+    r=[]
+    for x in k:
+        r = r +[int(x)]
+    accum = accum + [r]
+
+result = sorted(accum, key=sortKey)
+
+#OUTPUT
+try:
+    g = open(FOUT, 'w')
+    for i in range(len(result)):
+        for j in range(len(result[0])):
+            if j != len(result[0]) - 1:
+                g.write(str(result[i][j])+',')
+            else:
+                g.write(str(result[i][j])+'\n')
+except:
+    print 'ERR: cannot write to file "dataout"'
+
+
 
