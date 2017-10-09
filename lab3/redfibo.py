579d2106f1d9ea13df9700f0345f4029aca7d7bc
Louis-He <1726110778@qq.com>
2017-10-09 13:21:43 -0400

    add lab3 partD

---
+++ lab3/redfibo.py
@@ -0,0 +1,21 @@
+def fibo(n):
+    if n==0:
+        return 1
+    elif n==1:
+        return 1
+    else:
+        return fibo(n-1)+fibo(n-2)
+
+def fibol(n):
+    a=[]
+    for i in range (0,n+1):
+        a=a+[fibo(i)]
+    return a
+
+def redfibo(n):
+    def product(a,b):
+         return a*b
+    return reduce(product,fibol(n))
+
+
+
