63c27cade4bb356c4d53a3413394ed533e245cfe
Louis-He <1726110778@qq.com>
2017-10-09 13:17:12 -0400

    add lab1

---
+++ lab1/lab1.py
@@ -0,0 +1,25 @@
+def summl(n):
+    r=0
+    Neg=True
+    for i in range(1,n+1):
+        if Neg:
+          r=r-i
+          Neg=False
+        else:
+          r=r+i
+          Neg=True
+     return r
+
+
+def mult(A,B):
+   r=1
+   for i in range(0,B):
+     r=r*A
+     i=i+1
+   return r
+
+def isprime(x):
+   r=True
+   for i in range(2,x):
+     if (x%i=True):
+        
